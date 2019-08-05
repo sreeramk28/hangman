4e60681bdabcc0fab20cc8e470ae381867bfacab
sreeramk28 <43648958+sreeramk28@users.noreply.github.com>
2019-08-05 20:59:24 +0530

    Update HangMan.java

--- HangMan.java
+++ HangMan.java
@@ -11,7 +11,7 @@ import java.awt.event.ActionListener;
 
 class HangMan extends JFrame  {
 	public static Integer num_mistakes = 0;
-	private JLabel mist; 
+	private JLabel mist, over; 
 	private JPanel jp, jp1;
 	private JTextField mistake_count, letter0, letter1, letter2, letter3, letter4; 
 	private JTextField guess;
@@ -22,6 +22,8 @@ class HangMan extends JFrame  {
 		jp = new JPanel();
 		jp1 = new JPanel();		
 		mist = new JLabel("Mistakes");
+		over = new JLabel("");
+		over.setHorizontalAlignment(JLabel.LEFT);
 		mistake_count = new JTextField(5);
 		mistake_count.setHorizontalAlignment(JTextField.RIGHT);
 		/*guess*/
@@ -34,35 +36,27 @@ class HangMan extends JFrame  {
 		letter2 = new JTextField(2);
 		letter3 = new JTextField(2);
 		letter4 = new JTextField(2);
-		//for(int i=0; i<5; i++) {
-			letter0.setHorizontalAlignment(JTextField.LEFT);
-			letter0.setEditable(true);
-		//}	
+		letter0.setHorizontalAlignment(JTextField.LEFT);
+		letter0.setEditable(false);	
 		letter1.setHorizontalAlignment(JTextField.LEFT);
-		//letter1.setEditable(true);
+		letter1.setEditable(false);
 		letter2.setHorizontalAlignment(JTextField.LEFT);
-		//letter2.setEditable(true);
+		letter2.setEditable(false);
 		letter3.setHorizontalAlignment(JTextField.LEFT);
-		//letter3.setEditable(true);
+		letter3.setEditable(false);
 		letter4.setHorizontalAlignment(JTextField.LEFT);
-		//letter4.setEditable(true);
-		//letter5.setHorizontalAlignment(JTextField.LEFT);
-		//letter5.setEditable(true);
-		
-			//password.setHorizontalAlignment(JTextField.LEFT);
-			//password.setEditable(true);
+		letter4.setEditable(false);
 		submit.addActionListener(new ListenToSubmit());
 		jp1.add(letter0);
 		jp1.add(letter1);
 		jp1.add(letter2);
 		jp1.add(letter3);
 		jp1.add(letter4);
-		
 		jp1.add(mist);
 		jp1.add(mistake_count);
+		jp1.add(over);
 		jp.add(guess);
 		jp.add(submit);
-		//add(jp); 
 		add(jp1, BorderLayout.NORTH);
 		add(jp, BorderLayout.SOUTH);
 	}
@@ -75,24 +69,19 @@ class HangMan extends JFrame  {
 			char let = guess.getText().charAt(0);
 			String p = "";
 			for(int i=0; i<ww.length(); i++) {
-				//String p= "";
 				if(ww.charAt(i) == let) {
 					f = 1;
-					/*switch(i) {
-						case 0 : letter0.setText(p += let); break;
-						case 1 : letter1.setText(p += let); break;
-						case 2 : letter2.setText(p += let); break;
-						case 3 : letter3.setText(p += let); break;
-						case 4 : letter4.setText(p += let); break;
-					}*/
 					pos[k++] = i;
 				}
 			}
-			//for(int i=0; i<k; i++) System.out.print(pos[i]);
-			//System.out.println();
 			if(f == 0) {
 				num_mistakes++;
 				mistake_count.setText(num_mistakes.toString());
+				if(num_mistakes == 6) {
+					over.setText("GAME OVER, YOU LOST!");
+					submit.setVisible(false);
+					guess.setVisible(false);
+				}
 				
 			}	
 			else {
@@ -105,17 +94,29 @@ class HangMan extends JFrame  {
 						case 3 : letter3.setText(p += let); break;
 						case 4 : letter4.setText(p += let); break;
 					}
-				}	
+				}
+				String l1 = letter1.getText();
+				String l2 = letter2.getText();
+				String l3 = letter3.getText();
+				String l4 = letter4.getText();
+				String l0 = letter0.getText();
+				if(l1.length() > 0 && l2.length() > 0 
+					&& l3.length() > 0 && l4.length() > 0 
+					&& l0.length() > 0 ) {
+					over.setText("CONGRATS, YOU WON!!");
+					submit.setVisible(false);
+					guess.setVisible(false);
+				}			
 			}	
 			
 		}
 	}	
 	public static void main(String[] args) {
 		HangMan l = new HangMan();
-		l.setSize(600, 400);
+		l.setSize(450, 300);
 		//l.pack();
 		l.setLocationRelativeTo(null);
-		l.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		l.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 		l.setVisible(true);
 	}
 		
