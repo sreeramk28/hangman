1b5894fa835e192c5f1e2dc3e1ecb7429c5cce7a
sreeramk28 <43648958+sreeramk28@users.noreply.github.com>
2019-08-13 12:02:31 +0530

    Update HangMan.java

--- HangMan.java
+++ HangMan.java
@@ -6,12 +6,64 @@
 
 /**
  *
- * @author arun_y99, sreeramk100
+ * @author arun_y99, sreeramk100, (c) 2019
  */
 import java.awt.BorderLayout;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Line2D;
+
+class Bodyparts extends JPanel {
+
+    int mistake_number;
+
+    public void paintBody(int mistake_num) {
+        mistake_number = mistake_num;
+    }
+    @Override
+    public void paint(Graphics g) {
+        /*parts drawing*/
+        super.paint(g);
+        Graphics2D g2 = (Graphics2D) g;
+        if (mistake_number == 0) {
+            return;
+        }
+        if (mistake_number >= 1) {
+            Ellipse2D eli;
+            eli = new Ellipse2D.Double(240, 62, 20, 20);
+            g2.draw(eli);
+        }
+        if (mistake_number >= 2) {
+            Line2D blin;
+            blin = new Line2D.Double(250, 82, 250, 102);
+            g2.draw(blin);
+        }
+        if (mistake_number >= 3) {
+            Line2D blin;
+            blin = new Line2D.Double(250, 102, 230, 122);
+            g2.draw(blin);
+        }
+        if (mistake_number >= 4) {
+            Line2D blin;
+            blin = new Line2D.Double(250, 102, 270, 122);
+            g2.draw(blin);
+        }
+        if (mistake_number >= 5) {
+            Line2D blin;
+            blin = new Line2D.Double(250, 92, 230, 82);
+            g2.draw(blin);
+        }
+        if (mistake_number >= 6) {
+            Line2D blin;
+            blin = new Line2D.Double(250, 92, 270, 82);
+            g2.draw(blin);
+        }
+    }
+}
 
 class Hangman extends JFrame {
 
@@ -22,9 +74,11 @@ class Hangman extends JFrame {
     private JTextField[] letter = new JTextField[ww.length()];
     private JTextField guess;
     private JButton submit;
-    public static String ww = "SRINIVASA_ARUN";
+    public static String ww = "HELLO";
+    Bodyparts bp;
 
     public Hangman() {
+
         submit = new JButton("GUESS");
         jp = new JPanel();
         jp1 = new JPanel();
@@ -55,10 +109,30 @@ class Hangman extends JFrame {
         jp.add(submit);
         add(jp1, BorderLayout.NORTH);
         add(jp, BorderLayout.SOUTH);
+        bp = new Bodyparts();
+        add(bp);
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        /* Gallow Drawing */
+        super.paint(g);
+        Graphics2D g2 = (Graphics2D) g;
+        Line2D lin;
+        lin = new Line2D.Float(300, 100, 300, 250);
+        g2.draw(lin);
+        lin = new Line2D.Float(260, 100, 300, 100);
+        g2.draw(lin);
+        lin = new Line2D.Float(260, 100, 260, 120);
+        g2.draw(lin);
+        lin = new Line2D.Float(280, 250, 320, 250);
+        g2.draw(lin);
+
     }
 
     class ListenToSubmit implements ActionListener {
 
+        @Override
         public void actionPerformed(ActionEvent ae) {
             int f = 0;
             int[] pos = new int[50];
@@ -74,6 +148,8 @@ class Hangman extends JFrame {
             if (f == 0) {
                 num_mistakes++;
                 mistake_count.setText(num_mistakes.toString());
+                bp.paintBody(num_mistakes);
+                repaint();
                 if (num_mistakes == 6) {
                     over.setText("GAME OVER, YOU LOST!");
                     submit.setVisible(false);
@@ -96,12 +172,12 @@ class Hangman extends JFrame {
                     guess.setVisible(false);
                 }
             }
+            repaint();
         }
     }
-
     public static void main(String[] args) {
         Hangman l = new Hangman();
-        l.setSize(450, 300);
+        l.setSize(750, 563);
         l.setLocationRelativeTo(null);
         l.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
         l.setVisible(true);
